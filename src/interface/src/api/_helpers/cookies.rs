736c2035b7caa85f1c671a6b0c632cd686bd1ca9
Frqnku <ugo.tiberto@gmail.com>
2026-03-05 17:21:56 +0100

    feat(cookie): Added Cookie helper
    - Added a Cookie struct with builder pattern to create cookies and add them to response headers

---
+++ src/interface/src/api/_helpers/cookies.rs
@@ -0,0 +1,59 @@
+#[cfg(feature = "ssr")]
+use axum::http::header::{SET_COOKIE, HeaderValue};
+#[cfg(feature = "ssr")]
+use leptos_axum::ResponseOptions;
+
+pub enum CookieOptions {
+    HttpOnly,
+    Secure,
+    SameSiteLax,
+    SameSiteStrict,
+    SameSiteNone,
+    Path(String),
+    MaxAge(i64),
+    Expires(String),
+}
+
+pub struct Cookie {
+    pub name: String,
+    pub value: String,
+    pub options: Vec<CookieOptions>,
+}
+
+impl Cookie {
+    pub fn new(name: &str, value: &str) -> Self {
+        Self {
+            name: name.to_string(),
+            value: value.to_string(),
+            options: vec![],
+        }
+    }
+
+    pub fn with_options(mut self, options: Vec<CookieOptions>) -> Self {
+        self.options = options;
+        self
+    }
+
+    #[cfg(feature = "ssr")]
+    pub fn into_response_headers(&self, res: &ResponseOptions) -> () {
+        let cookie = build_cookie(&self.name, &self.value, &self.options);
+        res.insert_header(SET_COOKIE, HeaderValue::from_str(&cookie).unwrap());
+    }
+}
+
+fn build_cookie(name: &str, value: &str, options: &[CookieOptions]) -> String {
+    let mut cookie = format!("{}={}", name, value);
+    for option in options {
+        match option {
+            CookieOptions::HttpOnly => cookie.push_str("; HttpOnly"),
+            CookieOptions::Secure => cookie.push_str("; Secure"),
+            CookieOptions::SameSiteLax => cookie.push_str("; SameSite=Lax"),
+            CookieOptions::SameSiteStrict => cookie.push_str("; SameSite=Strict"),
+            CookieOptions::SameSiteNone => cookie.push_str("; SameSite=None"),
+            CookieOptions::Path(path) => cookie.push_str(&format!("; Path={}", path)),
+            CookieOptions::MaxAge(seconds) => cookie.push_str(&format!("; Max-Age={}", seconds)),
+            CookieOptions::Expires(date) => cookie.push_str(&format!("; Expires={}", date)),
+        }
+    }
+    cookie
+}
