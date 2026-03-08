bf7917ee91f82dd0bed335e8d28dc84ec24b828c
Frqnku <ugo.tiberto@gmail.com>
2026-03-08 16:00:15 +0100

    chores(icons): sorted icons for clearness

--- src/interface/src/components/ui/icons/icons.rs
+++ src/interface/src/components/ui/icons/icons.rs
@@ -1,21 +1,20 @@
-use leptos::{prelude::*, svg::view};
+use leptos::prelude::*;
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum IconKind {
-    Moon,
-    Translate,
-    Globe,
-    Settings,
-    LinkedIn,
-    Search,
-    Menu,
-    ChevronRight,
     ChevronLeft,
+    ChevronRight,
     DotMenu,
-    Logout,
+    Globe,
+    History,
     Home,
     HomeSelected,
-    History,
+    LinkedIn,
+    Logout,
+    Menu,
+    Moon,
+    Search,
+    Settings,
     ThumbsUp,
     ThumbsUpSelected,
 }
@@ -23,43 +22,21 @@ pub enum IconKind {
 #[component]
 pub fn Icon(kind: IconKind) -> impl IntoView {
     match kind {
-        IconKind::Moon => view! { <MoonIcon /> }.into_any(),
-        IconKind::Translate => view! { <TranslateIcon /> }.into_any(),
-        IconKind::Globe => view! { <GlobeIcon /> }.into_any(),
-        IconKind::Settings => view! { <SettingsIcon /> }.into_any(),
-        IconKind::LinkedIn => view! { <LinkedInIcon /> }.into_any(),
-        IconKind::Search => view! { <SearchIcon /> }.into_any(),
-        IconKind::Menu => view! { <MenuIcon /> }.into_any(),
-        IconKind::ChevronRight => view! { <ChevronRightIcon /> }.into_any(),
         IconKind::ChevronLeft => view! { <ChevronLeftIcon /> }.into_any(),
+        IconKind::ChevronRight => view! { <ChevronRightIcon /> }.into_any(),
         IconKind::DotMenu => view! { <DotMenuIcon /> }.into_any(),
-        IconKind::Logout => view! { <LogoutIcon /> }.into_any(),
+        IconKind::Globe => view! { <GlobeIcon /> }.into_any(),
+        IconKind::History => view! { <HistoryIcon /> }.into_any(),
         IconKind::Home => view! { <HomeIcon /> }.into_any(),
         IconKind::HomeSelected => view! { <HomeSelectedIcon /> }.into_any(),
-        IconKind::History => view! { <HistoryIcon /> }.into_any(),
+        IconKind::LinkedIn => view! { <LinkedInIcon /> }.into_any(),
+        IconKind::Logout => view! { <LogoutIcon /> }.into_any(),
+        IconKind::Menu => view! { <MenuIcon /> }.into_any(),
+        IconKind::Moon => view! { <MoonIcon /> }.into_any(),
+        IconKind::Search => view! { <SearchIcon /> }.into_any(),
+        IconKind::Settings => view! { <SettingsIcon /> }.into_any(),
         IconKind::ThumbsUp => view! { <ThumbsUpIcon /> }.into_any(),
         IconKind::ThumbsUpSelected => view! { <ThumbsUpSelectedIcon /> }.into_any(),
-    }
-}
-
-#[component]
-fn LogoutIcon() -> impl IntoView {
-    view! {
-        <svg xmlns="http://www.w3.org/2000/svg" class="" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path><polyline points="16 17 21 12 16 7"></polyline><line y2="12" y1="12" x1="21" x2="9"></line></svg>
-    }
-}
-
-#[component]
-fn DotMenuIcon() -> impl IntoView {
-    view! {
-        <svg xmlns="http://www.w3.org/2000/svg" class="" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><circle r="1" cy="12" cx="12"></circle><circle cx="12" r="1" cy="5"></circle><circle r="1" cy="19" cx="12"></circle></svg>
-    }
-}
-
-#[component]
-fn ChevronRightIcon() -> impl IntoView {
-    view! {
-        <svg xmlns="http://www.w3.org/2000/svg" class="" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6"></path></svg>
     }
 }
 
@@ -71,16 +48,16 @@ fn ChevronLeftIcon() -> impl IntoView {
 }
 
 #[component]
-fn MoonIcon() -> impl IntoView {
+fn ChevronRightIcon() -> impl IntoView {
     view! {
-        <svg xmlns="http://www.w3.org/2000/svg" class="" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"></path></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" class="" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6"></path></svg>
     }
 }
 
 #[component]
-fn TranslateIcon() -> impl IntoView {
+fn DotMenuIcon() -> impl IntoView {
     view! {
-        <svg xmlns="http://www.w3.org/2000/svg" class="" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><path d="m5 8 6 6"></path><path d="m4 14 6-6 2-3"></path><path d="M2 5h12"></path><path d="M7 2h1"></path><path d="m22 22-5-10-5 10"></path><path d="M14 18h6"></path></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" class="" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><circle r="1" cy="12" cx="12"></circle><circle cx="12" r="1" cy="5"></circle><circle r="1" cy="19" cx="12"></circle></svg>
     }
 }
 
@@ -88,34 +65,6 @@ fn TranslateIcon() -> impl IntoView {
 fn GlobeIcon() -> impl IntoView {
     view! {
         <svg xmlns="http://www.w3.org/2000/svg" class="" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" r="10" cy="12"></circle><path d="M12 2a14.5 14.5 0 0 0 0 20 14.5 14.5 0 0 0 0-20"></path><path d="M2 12h20"></path></svg>
-    }
-}
-
-#[component]
-fn SettingsIcon() -> impl IntoView {
-    view! {
-        <svg xmlns="http://www.w3.org/2000/svg" class="" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><path d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z"></path><circle cx="12" cy="12" r="3"></circle></svg>
-    }
-}
-
-#[component]
-fn LinkedInIcon() -> impl IntoView {
-    view! {
-        <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 448 512" width="20" height="20" xmlns="http://www.w3.org/2000/svg"><path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"></path></svg>
-    }
-}
-
-#[component]
-fn SearchIcon() -> impl IntoView {
-    view! {
-        <svg xmlns="http://www.w3.org/2000/svg" class="" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><circle cx="11" cy="11" r="8"></circle><path d="m21 21-4.3-4.3"></path></svg>
-    }
-}
-
-#[component]
-fn MenuIcon() -> impl IntoView {
-    view! {
-        <svg xmlns="http://www.w3.org/2000/svg" class="" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><line x2="20" y2="12" x1="4" y1="12"></line><line y1="6" x1="4" x2="20" y2="6"></line><line y2="18" x2="20" y1="18" x1="4"></line></svg>
     }
 }
 
@@ -137,6 +86,48 @@ fn HomeIcon() -> impl IntoView {
 fn HomeSelectedIcon() -> impl IntoView {
     view! {
         <svg xmlns="http://www.w3.org/2000/svg" class="" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true"><path d="m11.485 2.143-8 4.8-2 1.2a1 1 0 001.03 1.714L3 9.567V20a2 2 0 002 2h5v-8h4v8h5a2 2 0 002-2V9.567l.485.29a1 1 0 001.03-1.714l-2-1.2-8-4.8a1 1 0 00-1.03 0Z"></path></svg>
+    }
+}
+
+#[component]
+fn LinkedInIcon() -> impl IntoView {
+    view! {
+        <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 448 512" width="20" height="20" xmlns="http://www.w3.org/2000/svg"><path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"></path></svg>
+    }
+}
+
+#[component]
+fn LogoutIcon() -> impl IntoView {
+    view! {
+        <svg xmlns="http://www.w3.org/2000/svg" class="" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path><polyline points="16 17 21 12 16 7"></polyline><line y2="12" y1="12" x1="21" x2="9"></line></svg>
+    }
+}
+
+#[component]
+fn MenuIcon() -> impl IntoView {
+    view! {
+        <svg xmlns="http://www.w3.org/2000/svg" class="" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><line x2="20" y2="12" x1="4" y1="12"></line><line y1="6" x1="4" x2="20" y2="6"></line><line y2="18" x2="20" y1="18" x1="4"></line></svg>
+    }
+}
+
+#[component]
+fn MoonIcon() -> impl IntoView {
+    view! {
+        <svg xmlns="http://www.w3.org/2000/svg" class="" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"></path></svg>
+    }
+}
+
+#[component]
+fn SearchIcon() -> impl IntoView {
+    view! {
+        <svg xmlns="http://www.w3.org/2000/svg" class="" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><circle cx="11" cy="11" r="8"></circle><path d="m21 21-4.3-4.3"></path></svg>
+    }
+}
+
+#[component]
+fn SettingsIcon() -> impl IntoView {
+    view! {
+        <svg xmlns="http://www.w3.org/2000/svg" class="" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><path d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z"></path><circle cx="12" cy="12" r="3"></circle></svg>
     }
 }
 
